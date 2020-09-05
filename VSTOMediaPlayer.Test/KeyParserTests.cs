b91ab68f9ddd7a4d9877a13d4d9ecb6ce9ab6138
Jethro <jethrobar@gmail.com>
2020-09-05 15:24:40 +0200

    Create StringToKey and KeyToString methods for HeyParses helper class.

--- VSTOMediaPlayer.Test/KeyParserTests.cs
+++ VSTOMediaPlayer.Test/KeyParserTests.cs
@@ -1,4 +1,6 @@
 ﻿using Castle.Components.DictionaryAdapter;
+using MahApps.Metro.IconPacks;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using System;
 using System.Collections;
@@ -15,27 +17,81 @@ namespace VSTOMediaPlayer.Test
         [TestCase("E", Key.E)]
         [TestCase("OemPlus", Key.OemPlus)]
         [TestCase("Oem1", Key.Oem1)]
-        public void Parse_Single_Key(string input, Key expected)            
+        public void StringToKey_SingleKeyString_KeyWithoutModifier(string input, Key expected)            
         {
-            (Key key, ModifierKeys mod) actual = KeyParser.Parse(input);
+            (Key key, ModifierKeys mod) actual = KeyParser.StringToKey(input);
 
             Assert.That(expected, Is.EqualTo(actual.key));
         }
 
         [Test]
-        public void Parse_Exception_On_Invalid_Argument()
+        public void StringToKey_InvalidSingleKey_ThrowsArgumentException()
         {
-            string expectedMsg = "Requested value 'Bad input' was not found.";
-            Assert.Throws(Is.TypeOf<ArgumentException>().And.Message.EqualTo(expectedMsg), () => KeyParser.Parse("Bad input"));
+            string expectedMsg = $"Cannot convert \"Bad input\" to a valid Key value.";
+            Assert.Throws(Is.TypeOf<ArgumentException>().And.Message.EqualTo(expectedMsg), () => KeyParser.StringToKey("Bad input"));
         }
 
-        [TestCase("Ctrl+E", ModifierKeys.Control, Key.E)]
-        public void Parse_Key_Combination(string input, ModifierKeys expectedMod, Key expectedKey)
+        [Test]
+        public void StringToKey_InvalidModifierKey_ThrowsArgumentException()
         {
-            (Key key, ModifierKeys mod) actual = KeyParser.Parse(input);
+            string expectedMsg = $"Cannot convert \"bad\" to a valid ModifierKeys value.";
+            Assert.Throws(Is.TypeOf<ArgumentException>().And.Message.EqualTo(expectedMsg), () => KeyParser.StringToKey("bad+value"));
+        }
+
+        [TestCase("Control+E", ModifierKeys.Control, Key.E)]
+        [TestCase("Shift+Oem1", ModifierKeys.Shift, Key.Oem1)]
+        [TestCase("Alt+F1", ModifierKeys.Alt, Key.F1)]
+        [TestCase("Control+D1", ModifierKeys.Control, Key.D1)]
+        public void StringToKey_Key_Combination(string input, ModifierKeys expectedMod, Key expectedKey)
+        {
+            (Key key, ModifierKeys mod) actual = KeyParser.StringToKey(input);
 
             Assert.That(expectedMod, Is.EqualTo(actual.mod));
             Assert.That(expectedKey, Is.EqualTo(actual.key));
+        }
+
+        [TestCase("1", Key.D1, ModifierKeys.None)]
+        [TestCase("Shift+2", Key.D2, ModifierKeys.Shift)]
+        [TestCase("Control+3", Key.D3, ModifierKeys.Control)]
+        [TestCase("Alt+4", Key.D4, ModifierKeys.Alt)]
+        public void StringToKey_NumberedKeys_ParseToLetterDPrefixKeyEnum(
+            string input, Key expectedKey, ModifierKeys expectedModifier)
+        {
+            (Key key, ModifierKeys mod) actual = KeyParser.StringToKey(input);
+
+            Assert.That(actual.key, Is.EqualTo(expectedKey));
+            Assert.That(actual.mod, Is.EqualTo(expectedModifier));
+        }
+
+        [TestCase(Key.E, "E")]
+        [TestCase(Key.Oem1, "Oem1")]
+        [TestCase(Key.F1, "F1")]
+        public void KeyToString_SingleKeyEnum_ReturnSingleString(Key inputKey, string expected)
+        {
+            var actual = KeyParser.KeyToString(inputKey);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [TestCase(Key.E, ModifierKeys.Control, "Control+E")]
+        [TestCase(Key.F1, ModifierKeys.Alt, "Alt+F1")]
+        [TestCase(Key.OemPlus, ModifierKeys.Shift, "Shift+OemPlus")]
+        public void KeyToString_KeyGesture_ReturnGestureString(Key inputKey, ModifierKeys inputModifier, string expected)
+        {
+            var actual = KeyParser.KeyToString(inputKey, inputModifier);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [TestCase(Key.D1, ModifierKeys.None, "1")]
+        [TestCase(Key.D2, ModifierKeys.Shift, "Shift+2")]
+        [TestCase(Key.D3, ModifierKeys.Control, "Control+3")]
+        [TestCase(Key.D4, ModifierKeys.Alt, "Alt+4")]
+        public void KeyToString_DNumberedKey_RemovesLetterDPrefix(Key inputKey, ModifierKeys inputModifier, string expected)
+        {
+            var actual = KeyParser.KeyToString(inputKey, inputModifier);
+
+            Assert.That(expected, Is.EqualTo(actual));
         }
     }
 }
--- VSTOMediaPlayer.Word/Configuration/KeyParser.cs
+++ VSTOMediaPlayer.Word/Configuration/KeyParser.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -9,13 +11,40 @@ namespace VSTOMediaPlayer.Word.Configuration
 {
     public static class KeyParser
     {
-        public static (Key, ModifierKeys) Parse(string keyValue)
+        public static (Key, ModifierKeys) StringToKey(string keyValue)
         {
             (Key key, ModifierKeys mod) result = (0, 0);
 
             var items = keyValue.Split('+');
 
-            result.key = (Key)Enum.Parse(typeof(Key), items[1]);
+            if (items.Length > 1 && items.Length < 5)
+            {
+                for (int i = 0; i < items.Length - 1; i++)
+                {
+                    _ = Enum.TryParse(items[i], out result.mod) ? true : throw new ArgumentException(
+                        $"Cannot convert \"{items[i]}\" to a valid ModifierKeys value.");
+                }
+            }
+
+            if (Regex.IsMatch(items.Last(), @"^\d$"))
+                items[items.Length - 1] = "D" + items.Last();
+
+            _ = Enum.TryParse(items.Last(), out result.key) ? true : throw new ArgumentException(
+                $"Cannot convert \"{items.Last()}\" to a valid Key value.");
+            return result;
+        }
+
+        public static string KeyToString(Key key, ModifierKeys modifier = ModifierKeys.None)
+        {
+            string result = string.Empty;
+
+            if (modifier != ModifierKeys.None)
+                result = modifier.ToString() + "+";
+
+            if (Regex.IsMatch(key.ToString(), @"^D\d$"))
+                result += key.ToString().Remove(0, 1);
+            else
+                result += key.ToString();
 
             return result;
         }
